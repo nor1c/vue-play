f673757255e3a01a0d4318d0bee97994bff96391
norictech <ahmad.uji1902@gmail.com>
2021-09-04 22:08:37 +0700

    hello world & counter

---
+++ hello-world/src/views/Counter.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="counter">
+        Counter: {{ counter }}
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            counter: 0
+        }
+    },
+    mounted() {
+        this.counterval()
+    },
+    methods: {
+        counterval: function () {
+            setInterval(() => {
+                this.counter++
+            }, 200);
+        }
+    }
+}
+</script>
