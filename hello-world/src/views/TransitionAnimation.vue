5c3d9830793e0c19b1adf76b72865481d654276c
norictech <ahmad.uji1902@gmail.com>
2021-09-07 21:09:54 +0700

    list entering/leaving transitions

--- hello-world/src/views/TransitionAnimation.vue
+++ hello-world/src/views/TransitionAnimation.vue
@@ -34,6 +34,20 @@
         </transition>
       </div>
     </div>
+
+    <hr>
+    <div>
+      <h3>List Entering/Leaving Transitions</h3>
+      <div>
+        <button @click="add">Add</button>
+        <button @click="remove">Remove</button>
+        <transition-group name="list" tag="p">
+          <span v-for="item in items" :key="item" class="list-item">
+            {{ item }}
+          </span>
+        </transition-group>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -42,11 +56,22 @@ export default {
   data: () => ({
     noActivated: false,
     x: 0,
-    show: false
+    show: false,
+    items: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    nextNum: 10
   }),
   methods: {
     xCoordinate(e) {
       this.x = e.clientX
+    },
+    randomIndex() {
+      return Math.floor(Math.random() * this.items.length)
+    },
+    add() {
+      this.items.splice(this.randomIndex(), 0, this.nextNum++)
+    },
+    remove() {
+      this.items.splice(this.randomIndex(), 1)
     }
   }
 }
@@ -93,5 +118,18 @@ export default {
 .fade-enter-from,
 .fade-leave-to {
   opacity: 0;
+}
+.list-item {
+  display: inline-block;
+  margin-right: 10px;
+}
+.list-enter-active,
+.list-leave-active {
+  transition: all 1s ease;
+}
+.list-enter-from,
+.list-leave-to {
+  opacity: 0;
+  transform: translateY(30px);
 }
 </style>
