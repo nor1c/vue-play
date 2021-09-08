0ab7dc8efa7c9d87577c8a0e1d3e712c08d22ad2
norictech <ahmad.uji1902@gmail.com>
2021-09-08 16:39:07 +0700

    custom option merge strategies

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,23 @@ import router from './router'
 
 const app = createApp({
   globalMixinHello: 'I come from global option (mixin)',
+  custom: 'I am a custom option.',
   render: () => h(App)
 })
+app.config.optionMergeStrategies.custom = (toVal, fromVal) => {
+  // I am a custom option., undefined
+  // (new custom option value), I am a custom option (old value)
+  return fromVal || toVal
+}
 app.mixin({
+  custom: 'I am a new custom option value.',
   created() {
     const globalMixinHello = this.$options.globalMixinHello
     if (globalMixinHello) {
       console.log(globalMixinHello)
     }
+
+    console.log(this.$options.custom)
   }
 })
 app.use(router)
